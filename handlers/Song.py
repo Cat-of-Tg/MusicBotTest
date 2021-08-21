d1865061785ec8460b3af886196cc097109f5f88
POISON <86126808+PoisonBot@users.noreply.github.com>
2021-08-21 19:22:03 +0530

    Update Song.py

--- handlers/Song.py
+++ handlers/Song.py
@@ -20,7 +20,7 @@ from youtube_dl.utils import (
 
 @Client.on_message(filters.command("song") & ~filters.edited)
 async def song(client, message):
-    cap = "@warbotz"
+    cap = "@teamladz_bothub"
     url = message.text.split(None, 1)[1]
     rkp = await message.reply("Processing...")
     if not url:
